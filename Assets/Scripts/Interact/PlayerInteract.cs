501171ca031f15fe0414cdaf64a1adf593407b66
Vimukthi Kulathilaka <94285196+VimukthiMK@users.noreply.github.com>
2023-12-07 23:38:24 +0530

    Minor UI changes & Added Popup Showing Game Instructions.

---
+++ Assets/Scripts/Interact/PlayerInteract.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    private Camera cam;
+
+    void Start()
+    {
+        
+    }
+}
+
+
