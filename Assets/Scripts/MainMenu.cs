6646512f2458e29b1cb675515748beed5b681497
Vimukthi Kulathilaka <94285196+VimukthiMK@users.noreply.github.com>
2023-12-08 02:19:56 +0530

    Added QuitButton Functionality to Main Menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -12,20 +12,10 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void OpenOptions()
-    {
-        SceneManager.LoadScene("OptionsScene"); // Replace with the name of your options scene
-    }
-
     public void QuitGame()
     {
         Application.Quit();
     }
 
-    // Add this method to be called when the player interacts with the collider
-    public void LoadNextScene()
-    {
-        SceneManager.LoadScene(gameSceneName);
-    }
 
 }
