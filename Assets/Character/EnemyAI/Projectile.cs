dc591bc77f6bf507b5477794d47a3fea38c5e90f
Vimukthi Kulathilaka <94285196+VimukthiMK@users.noreply.github.com>
2023-12-08 22:33:40 +0530

    Fixed Enemy AI

---
+++ Assets/Character/EnemyAI/Projectile.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public GameObject impactEffect;
+    private void OnCollisionEnter(Collision collision)
+    {
+        FindObjectOfType<AudioManager>().Play("Shoot");
+        GameObject impact = Instantiate(impactEffect, transform.position, Quaternion.identity);
+        Destroy(impact, 2);
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Player/Gun.cs
+++ Assets/Scripts/Player/Gun.cs
@@ -6,6 +6,7 @@ public class Gun : MonoBehaviour
     public Transform fpsCam;
     public float range = 20;
     public float impactForce = 150;
+    public int damageAmount = 20;
 
     public int fireRate = 10;
     private float nextTimeToFire = 0;
@@ -47,6 +48,14 @@ public class Gun : MonoBehaviour
             {
                 hit.rigidbody.AddForce(-hit.normal * impactForce);
             }
+            Enemy e = hit.transform.GetComponent<Enemy>();
+            if (e != null)
+            {
+                e.TakeDamage(damageAmount);
+                return;
+            }
+
+
 
             Quaternion impactRotation = Quaternion.LookRotation(hit.normal);
             GameObject impact = Instantiate(impactEffect, hit.point, impactRotation);
